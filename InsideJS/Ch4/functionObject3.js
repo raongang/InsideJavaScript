12de31d0255097355b72094c7116399047b3f699
raongang <sayllclubs@naver.com>
2019-10-14 17:11:27 +0900

    2019-10-14 raongang add

--- InsideJS/Ch4/functionObject3.js
+++ InsideJS/Ch4/functionObject3.js
@@ -22,9 +22,194 @@ func(1,2,3); // 1 2
 function add(a,b){
 	//arguments 객체 출력
 	console.log(arguments);
+	console.log("arguments.length : " + arguments.length); //인자개수..
 	return a+b;
 }
 console.log('ex4-22 arguments 유사 배열 객체 예제코드');
-console.log(add(1));
-console.log(add(1,2));
-console.log(add(1,2,3));
+console.log(add(1)); //Nan
+console.log(add(1,2)); //3
+console.log(add(1,2,3)); //3
+
+//ex
+function sum(){
+	var  result = 0;
+	
+	for(var i=0; i<arguments.length; i++){
+		result+= arguments[i];
+	}
+	
+	return result;
+}
+
+console.log(sum(1,2,3));
+console.log(sum(1,2,3,4,5,6,7,8,9));
+
+/**
+ *   4.4.2 호출패턴과 this 바인딩(객체참조)
+ *     - 자바스크립트에서는 함수를 호출할때 기존 매개변수로 전달되는 인자값에 더해, arguments 객체 및 this인자가 함수 내부로 암묵적으로 전달된다. 
+ *        
+ * */
+
+//ex 4-23 메소드 호출 사용시 this바인딩 - 자기 자신을 호출한 객체에 바인딩된다.
+
+//myObject 객체 생성
+var myObject = {
+	name : 'foo',
+	sayName : function(){ console.log(this.name); } 
+};
+
+//otherObject 객체 생성
+var otherObject = { 
+		name : 'bar' 
+ };
+
+//otherObject.sayName() 메소드  ( 객체 프로퍼티 동적 생성 ) 
+otherObject.sayName = myObject.sayName;
+
+//sayName call
+myObject.sayName(); // 'foo' 
+otherObject.sayName(); //'bar'
+
+/**
+ *  4.4.2.2 함수를 호출할 때 this 바인딩
+ *    - 자바스크립트에서는 함수 호출시, 해당 함수 내부 코드에서 사용된 this는 전역객체에 바인딩
+ *    - 브라우저에서 자바스크립트를 실행시, 전역객체는 window 객체
+ *    - 내부 함수도 결국 함수이기 떄문에 함수호출로 취급되면 this전역객체에 바인딩 된다.
+ */
+
+//ex 4-24  전역객체와 전역변수의 관계를 보여주는 예제.
+
+var foo = 'temp';
+console.log(foo);
+//console.log(window.foo);
+
+//ex4-25 함수를 호출할때 this 바인딩을 보여주는 예제.
+var test = 'This is test';
+//console.log(window.test);
+//sayFoo()함수
+var sayFoo = function(){
+	console.log(this.test);
+}
+sayFoo();
+
+
+//ex4-26 내부 함수의 this바인딩 동작을 보여주는 예제코드 (문제코드)
+console.log('ex4-26');
+//전역변수 value정의
+var value=100;
+//myObject 객체 생성
+var myObject2 ={
+		value : 1,
+		fun1 : function(){
+					this.value+=1;
+					console.log('fun1 called this.value : ' + this.value);
+			//fun2 내부함수 
+			fun2 = function(){
+				this.value+=1;
+				console.log('fun2() called. this.value : ' + this.value); //101
+		
+				//fun3 내부 함수
+				fun3 = function(){
+					this.value+=1;
+					console.log('fun3() called. this.value : ' + this.value); //102
+				}
+				
+				fun3();
+			}
+			fun2();
+		}//func1
+};
+
+myObject2.fun1();
+
+//ex4-27 내부 함수의 this 바인딩 문제를 해결한 예제코드 ( ※  보통 this를 저장하는 변수를 that으로 사용한다 ) 
+//내부 함수 this바인딩
+var value2=100;
+var myObject3 ={
+	value : 1,
+	fun11 : function(){
+				var that = this;
+				this.value+=1;
+				console.log('fun11() called this.value : ' + this.value); //2
+		//fun2 내부함수 
+		fun22 = function(){
+			that.value+=1;
+			console.log('fun22() called. this.value : ' + that.value); //3
+	
+			//fun3 내부 함수
+			fun33 = function(){
+				that.value+=1;
+				console.log('fun33() called. this.value : ' + that.value);  //4
+			}
+			fun33();
+		}
+		fun22();
+	}//end func11
+};
+
+myObject3.fun11();
+
+
+/**
+ *     4.4.2.3 생성자 함수를 호출할때 this 바인딩 (중요)
+ *       - 기존 함수에 new 연산자를 붙여서 호출하면 해당 함수는 생성자 함수로 동작함.
+ *       - 원치않는 생성자 함수의 호출을 방지하기 위해서 함수의 첫 문자를 대문자로 쓰기를 권고하고 있음.
+ *       - 생성자 함수가 생성한 객체는 자신을 생성한 생성자 함수의 prototype 프로퍼티가 가르키는 객체를
+ *         자신의 프로포타입객체로 설정한다. 
+ * */
+
+
+//ex 4-28 생성자 함수의 동작방식
+
+//Person() 생성자 함수 
+var Person = function(name){
+	//함수 코드 실행전
+	this.name = name;
+	//함수리턴
+}
+var con = new Person('con'); //con 객체 생성
+console.log(con.name);
+
+//ex 4-29 객체 생성 두 가지 방법(객체 리터럴 vs 생성자 함수) 
+
+//객체 리터럴 방식으로 foo객체 생성
+var literObj = {
+		name : 'literal',
+		age : 35,
+		gender : 'man'
+};
+console.dir(literObj);
+
+//생성자 함수 
+function Person2(name,age, gender,position){
+	this.name = name;
+	this.age=age;
+	this.gender=gender;
+}
+
+//Person2 생성자 함수를 이용해 bar객체, baz객체 생성 
+var bar = new Person2('bar',33,'woman');
+console.dir(bar);
+
+var baz = new Person2('baz',25,'woman');
+console.log(baz);
+
+//ex 4-30 new를 붙이지 않고 생성자 함수 호출시의 오류.
+
+var qux = Person2('qux',20,'man');
+console.log(qux); //undefined   this는 함수호출이므로 전역객체인 window 객체로 바인딩됨. 동적으로 생성된다.
+console.log(window.name);
+console.log(window.age);
+console.log(window.gender);
+
+
+
+
+
+
+
+
+
+
+
+
--- InsideJsSpringProject/src/main/webapp/resources/js/functionObject.js
+++ InsideJsSpringProject/src/main/webapp/resources/js/functionObject.js
@@ -1,125 +1,21 @@
-/**
- 4.2.1자바스크립트에서는 함수도 객체(일급객체)이다
-   - 함수도 객체처럼 프로퍼티를 가질 수 있다.
-*/
-function add(x,y){
-	return x+y;
+//생성자 함수 
+function Person2(name,age, gender,position){
+	this.name = name;
+	this.age=age;
+	this.gender=gender;
 }
 
-//프로퍼티 추가
-add.result = add(3,2);
-add.status='OK';
+//Person2 생성자 함수를 이용해 bar객체, baz객체 생성 
+var bar = new Person2('bar',33,'woman');
+console.dir(bar);
 
-console.log(add.result);
-console.log(add.status);
+var baz = new Person2('baz',25,'woman');
+console.log(baz);
 
+//ex 4-30 new를 붙이지 않고 생성자 함수 호출시의 오류.
 
-/** 4.2.2 자바스크립트에서 함수는 값을 취급된다. 
- *   - 변수나 프로퍼티 값으로 할당
- * */
-
-var foo=100; //변수에 값 할당
-var bar = function(){ return 100; };
-console.log(bar());
-
-var obj = {}; 
-//객체의 프로퍼티 동적 생성
-obj.baz = function () { return 200; }
-console.log(obj.baz());
- 
-
-/**
-	4.2.2.2 함수 인자로 전달
-	 - 함수를 다른 함수의 인자로 넘긴다
-*/
-
-var foo= function(func){ //함수표현식으로 foo()함수 생성
-	func();
-};
-
-//foo()함수 생성
-foo(function (){
-	console.log('Function can be used as the argument1');
-});
-
-/** 4.2.2.3 리턴값으로 활용 */
-		
-var foo2 = function temp2(){ //함수를 리턴하는 foo() 함수 정의
-	return function(){
-		var a = 5;
-		return a;
-		//console.log('this function is the return value');
-	};
-};
-
-var bar = foo2(); //foo()함수가 호출되면, 리턴값으로 전달되는 함수가 bar변수에 저장된다.
-console.log(bar());
-
-//var bar = function(){ console.log('this function is the return value2');};
-//bar(); // () 함수호출 연산자를 이용하여 bar()로 리턴된 함수를 실행하는 것이 가능하다.
-
-/** 4.2.3 함수 객체의 기본 프로퍼티
-	 - 함수는 객체이며 표준 프로퍼티가 정의되어 있다
-	 - 모든 함수들의 부모객체는 Function Prototype 객체.
-	 - Function Prototype 객체 그 자체는 예외적으로 부모가 자바스크립트의 모든 객체의 조상인 Object.prototype 객체임.
-*/
-
-//add()함수 객체 프로퍼티를 출력하는 코드
-function getFunctionProperty(x,y){
-	return x+y;
-}
-
-//console.dir(getFunctionProperty);
-
-
-/** 4.2.3.1 length property 
- *   - 함수 객체의 length 프로퍼티는 함수를 작성할 때 정의한 인자 개수.
- * */
-function funcLength(){}
-function funcLength1(x){ return x; }
-function funcLength2(x,y) { return x+y; }
-function funcLength3(x,y,z) { return x+y+z; }
-
-console.log("funcLength : " + funcLength.length);   //0
-console.log("funcLength1 : " + funcLength1.length); //1
-console.log("funcLength2 : " + funcLength2.length); //2
-console.log("funcLength3 : " + funcLength3.length); //3
-
-/** 4.2.3.2 prototype property (이 부분 다시 체크)
- *    - prototype 객체 네이밍은 자신과 연결된 함수의 prototype 프로퍼티 값을 그대로 이용한다.
- *      ( ex. 함수 이름이 add() 면 프로토타입 객체는 add.prototype
- * */ 
-
-//MyFunction()함수 정의
-
-function myFunction(){
-	return true;
-}
-
-//console.dir(myFunction.prototype); //객체네이밍
-//console.dir(myFunction.prototype.constructor);
-
-
-var foo2 = function temp(){ //함수를 리턴하는 foo() 함수 정의
-	return function temp2(){
-		var a = 5;
-		return a;
-		//console.log('this function is the return value');
-	};
-};
-
-var bar = foo2(); //foo()함수가 호출되면, 리턴값으로 전달되는 함수가 bar변수에 저장된다.
-console.log(bar);
-console.log(bar());
-
-
-
-
-
-
-
-
-
-
-
-
+var qux = Person2('qux',20,'man');
+console.log(qux);
+console.log(window.name);
+console.log(window.age);
+console.log(window.gender);
