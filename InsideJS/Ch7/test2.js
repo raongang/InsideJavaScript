d5f868b34086a4d8974304c4b58fe12252911538
raongang <sayllclubs@naver.com>
2019-11-05 10:01:08 +0900

    2019-11-05 raongang add

--- InsideJS/Ch7/test2.js
+++ InsideJS/Ch7/test2.js
@@ -1,25 +1,17 @@
 /**
  *  apply,call,bind
- *    
- */
-/**
- *
  *    - 자바스크립트에서는 함수 호출시, 해당 함수 내부 코드에서 사용된 this는 전역객체에 바인딩
  *    - 브라우저에서 자바스크립트를 실행시, 전역객체는 window 객체
  *    - 내부 함수도 결국 함수이기 떄문에 함수호출로 취급되면 this전역객체에 바인딩 된다.
- */
+
 	
-
-
 function Shape(){
 	this.x = 0;
 	this.y = 0;
 }
 
 Shape.prototype={
-		
 		move : function(x,y){
-			
 			//console.log(this);
 			var self = this;
 			
@@ -34,10 +26,48 @@ Shape.prototype={
 					console.log('warning');
 				}
 			}//end f
-			
 			checkBounds();
 		}//end m
 };
 
 var shape = new Shape();
 shape.move(101,1);
+ */
+
+function Shape(){
+	this.x = 0;
+	this.y = 0;
+}
+
+Shape.prototype={
+		move : function(x,y){
+			//console.log(this);
+			
+			this.x += x;
+			this.y += y;
+			
+			console.log(this.x);
+			console.log(this.y);
+			
+			function checkBounds(){
+				if(this.x > 100 ){
+					console.log('warning');
+				}
+			}//end f
+			checkBounds.call(this);
+		}//end m
+};
+
+var shape = new Shape();
+shape.move(101,1);
+
+
+
+
+
+
+
+
+
+
+
